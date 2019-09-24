8e5a2967300759ec8a2dc0773a99baa3bf1f1668
Peter Wiley <peter.c.wiley@gmail.com>
2019-09-24 19:08:37 -0400

    dev: fixed some last minute tweaks (round 1)

--- assets/custom.js
+++ assets/custom.js
@@ -74,6 +74,132 @@ var customScripts = function($){
     }
   });
 
+
+  /* instagram images on homepage -- change link */
+
+  $(document).on('click', 'a.rap__med__wrap__inside', function(e){ 
+    e.preventDefault(); 
+    var url = 'https://www.instagram.com/maneclubnyc/';//$(this).attr('href'); 
+    window.open(url, '_blank');
+  });
+
+
+  /* EU cookie banner */
+
+  var settings = {};
+
+  $.cookieMessage = function (options) {
+    var defaults = {
+      mainMessage: "",
+      acceptButton: "Accept",
+      expirationDays: 20,
+      backgroundColor: '#000',
+      fontSize: '16px',
+      fontColor: 'white',
+      btnBackgroundColor: '#fc0000',
+      btnFontSize: '16px',
+      btnFontColor: 'white',
+      linkFontColor: '#ffff00',
+      cookieName: 'cookieMessage'
+    };
+
+    settings = $.extend( {}, defaults, options );
+    ready();
+  }
+
+  function ready() {
+    var coo = getCookie(settings.cookieName);
+    if (coo != "true") {
+      $(document).ready(function() {
+        cookieMessageGenerate();
+      })
+    }
+  }
+
+  function setCookie(c_name, value, exdays) {
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
+    document.cookie = c_name + "=" + c_value;
+  }
+
+  function getCookie(c_name) {
+    var i, x, y, ARRcookies = document.cookie.split(";");
+    for (i = 0; i < ARRcookies.length; i++) {
+      x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+      y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+      x = x.replace(/^\s+|\s+$/g, "");
+      if (x == c_name) {
+        return unescape(y);
+      }
+    }
+  }
+
+  function cookieMessageGenerate() {
+    var html = '<div id="cookie-msg">'+
+      '<span class="msg">'+settings.mainMessage+
+      '<a href="" class="btn-accept">'+settings.acceptButton+'</a>'+
+      '</span></div>';
+
+    $("body").append(html);
+
+    $("#cookie-msg").css({
+      'position': 'fixed',
+      'bottom': '0',
+      'z-index': '999',
+      'width': '100%',
+      'text-align': 'center',
+      'padding': '30px 50px',
+      'background-color': settings.backgroundColor,
+      'color': settings.fontColor,
+      'font-size': settings.fontSize,
+    });
+
+    $("#cookie-msg a").css({
+      'color': settings.linkFontColor,
+      'text-decoration': 'underline',
+    });
+
+    $("#cookie-msg a.btn-accept").css({
+      'padding': '2px 8px',
+      'border-radius': '1px',
+      'margin-left': '0.5em',
+      'border': '1px solid white',
+      'background-color': settings.btnBackgroundColor,
+      'color': settings.btnFontColor,
+      'font-size': settings.btnFontSize,
+      'font-family': 'Poppins,sans-serif',
+      'font-weight': 'bold',
+      'text-decoration': 'none',
+      'cursor': 'pointer'
+    });
+
+    $("#cookie-msg a.btn-accept").hover(function(e){
+      $(this).css(
+        'background-color',
+        e.type === "mouseenter"?"#c90000":"#fc0000");
+    });
+
+    $("#cookie-msg a.btn-accept").on("click", function(){
+      var coo = setCookie(settings.cookieName, true, settings.expirationDays);
+      $("#cookie-msg").remove();
+
+      return false;
+    })
+  }
+
+  $.cookieMessage({
+    'mainMessage': 'This website uses cookies. By using this website you consent to our use of these cookies. ',
+    'acceptButton': 'GOT IT',
+    'fontSize': '16px',
+    'btnFontSize': '14px',
+    'backgroundColor': '#000',
+  });
+
+  $('a').filter(function() {
+   return this.hostname && this.hostname !== location.hostname;
+}).attr("target","_blank");
+
 };
 
 //
